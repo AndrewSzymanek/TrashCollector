6baee854aa83933dd099ff64dc492053ddbff249
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-25 15:20:22 -0600

    added customer and employee models, then migrated database

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int id { get; set; }
+
+        [DisplayName("Name")]
+        public string name { get; set; }
+
+        [DisplayName("Address")]
+        public string address { get; set; }
+
+        [DisplayName("City")]
+        public string city { get; set; }
+
+        [DisplayName("State")]
+        public string state { get; set; }
+
+        [DisplayName("Zip Code")]
+        public int zipCode { get; set; }
+
+        [DisplayName("Pickup Day")]
+        public string pickupDay { get; set; }
+
+        [DisplayName("Balance")]
+        public double balance { get; set; }
+
+        [DisplayName("Extra Pickup Day")]
+        public string extraPickupDay { get; set; }
+
+        [DisplayName("Start Date")]
+        public string startDate { get; set; }
+
+        [DisplayName("End Date")]
+        public string endDate { get; set; }
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+
+        [DisplayName ("Name")]
+        public string name { get; set; }
+        [DisplayName ("Zip Code")]
+        public int zipCode { get; set; }
+    }
+}
